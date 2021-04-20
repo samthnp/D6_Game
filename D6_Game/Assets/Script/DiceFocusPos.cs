a79a1152ea062ad22eed4dc85fb60f023e133c99
TP <thanaponpdm@gmail.com>
2021-04-20 19:51:47 +0700

    Finish dice movement functionality
    - clicking on the dice object will move them into focus
    - clicking on the already focus dice will remove them from the screen

--- D6_Game/Assets/Script/DiceFocusPos.cs
+++ D6_Game/Assets/Script/DiceFocusPos.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class DiceFocusPos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool focusDiceOccupy = false;
 }
--- D6_Game/Assets/Script/Player.cs
+++ D6_Game/Assets/Script/Player.cs
@@ -10,12 +10,20 @@ public class Player : MonoBehaviour
    public GameObject focusDice;
    public Transform focusDiceTransform;
 
+   public GameObject removeDice;
+   public Transform RemoveDiceTransform;
+
+   public DiceFocusPos diceFocusPos;
+
    public float diceFocusMoveSpeed;
 
    private void Start()
    {
-      focusDice = GameObject.FindGameObjectWithTag("FocusDice");
+      focusDice = GameObject.FindGameObjectWithTag("FocusTransform");
       focusDiceTransform = focusDice.transform;
+
+      removeDice = GameObject.FindGameObjectWithTag("RemoveDice");
+      RemoveDiceTransform = removeDice.transform;
    }
 
    private void Update()
@@ -25,26 +33,55 @@ public class Player : MonoBehaviour
 
    private void OnMouseClick()
    {
+      // run the condition when a mouse click happens
       if (Input.GetMouseButtonUp(0))
       {
          Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
          Vector2 mouseLocation2D = new Vector2(mouseLocation.x, mouseLocation.y);
          RaycastHit2D hit = Physics2D.Raycast(mouseLocation2D, Vector2.zero, 10f);
          
-         Debug.DrawLine(mouseLocation2D,Vector2.zero);
 
+         // Check object of raycast from mouse
          if (hit.collider != null)
          {
-            if (hit.collider.CompareTag("DiceObject"))
+            // move the dice from the grid into focus
+            if (hit.collider.CompareTag("DiceObject") && diceFocusPos.focusDiceOccupy == false)
             {
                Debug.Log("Hit something : " + hit.collider.name);
-               hit.transform.position = Vector2.Lerp(hit.transform.position, focusDiceTransform.position,
-                  diceFocusMoveSpeed);
+               // moving the dice into focus
 
+               hit.transform.position = 
+                  Vector2.Lerp(hit.transform.position, focusDiceTransform.position, diceFocusMoveSpeed);
+               Debug.Log("Move dice into focus");
+
+               Invoke("FocusIsOccupy", 0.5f);
+               
+               hit.transform.tag = "FocusDice";
+            }
+            
+            // removing the focus dice
+            if (hit.collider.CompareTag("FocusDice") && diceFocusPos.focusDiceOccupy == true)
+            {
+               // removing the dice
+               hit.transform.position =
+                  Vector2.Lerp(hit.transform.position, RemoveDiceTransform.position, diceFocusMoveSpeed);
+               Debug.Log("Removing dice");
+               
+               Invoke("FocusIsNotOccupy", 0.5f);
             }
          }
 
       }
+   }
+
+   private void FocusIsOccupy()
+   {
+      diceFocusPos.focusDiceOccupy = true;
+   }
+
+   private void FocusIsNotOccupy()
+   {
+      diceFocusPos.focusDiceOccupy = false;
    }
    
 }
