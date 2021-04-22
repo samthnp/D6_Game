5a5db6fc086823a40f5d61f8f0641a9ed6c2ecc0
TP <thanaponpdm@gmail.com>
2021-04-22 14:18:16 +0700

    Start the grid system approach
    - start setting up the grid

---
+++ D6_Game/Assets/DiceGrid.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceGrid : MonoBehaviour
+{
+    // 1st column
+    public GameObject upperSpawner1;
+    private Transform upperSpawn1Transform;
+    
+    public GameObject lowerSpawner1;
+    private Transform lowerSpawn1Transform;
+    
+    public GameObject hiddenSpawner1;
+    private Transform hiddenSpawn1Transform;
+
+    private bool lowerSpawn1_Empty = false;
+    
+    // 2rd column
+    public GameObject upperSpawner2;
+    private Transform upperSpawn2Transform;
+    
+    public GameObject lowerSpawner2;
+    private Transform lowerSpawn2Transform;
+    
+    private bool lowerSpawn2_Empty = false;
+    
+    public GameObject hiddenSpawner2;
+    private Transform hiddenSpawn2Transform;
+    
+    // 3rd column
+    public GameObject upperSpawner3;
+    private Transform upperSpawn3Transform;
+    
+    public GameObject lowerSpawner3;
+    private Transform lowerSpawn3Transform;
+    
+    public GameObject hiddenSpawner3;
+    private Transform hiddenSpawn3Transform;
+    
+    private bool lowerSpawn3_Empty = false;
+    
+    // 4th column
+    public GameObject upperSpawner4;
+    private Transform upperSpawn4Transform;
+    
+    public GameObject lowerSpawner4;
+    private Transform lowerSpawn4Transform;
+    
+    public GameObject hiddenSpawner4;
+    private Transform hiddenSpawn4Transform;
+    
+    private bool lowerSpawn4_Empty = false;
+    
+    // 5th column
+    public GameObject upperSpawner5;
+    private Transform upperSpawn5Transform;
+    
+    public GameObject lowerSpawner5;
+    private Transform lowerSpawn5Transform;
+    
+    public GameObject hiddenSpawner5;
+    private Transform hiddenSpawn5Transform;
+    
+    private bool lowerSpawn5_Empty = false;
+}
